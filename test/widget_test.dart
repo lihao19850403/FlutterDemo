8cd14fd0b922ec81ee07447e9812421d809ab1f1
li.hao85 <lihao_19850403@126.com>
2023-12-05 20:42:38 +0800

    Change main page name.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_demo_2023/marterial_app_demo.dart';
+import 'package:flutter_demo_2023/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
