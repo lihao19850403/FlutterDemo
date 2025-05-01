db1f62979b3314c1a174106b4f873be43d6ac423
li.hao85 <lihao_19850403@126.com>
2025-05-01 18:02:23 +0800

    Update content 7.

--- lib/base_gesture/custom_ttf.dart
+++ lib/base_gesture/custom_ttf.dart
@@ -6,19 +6,33 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: "加载字体",
-        home: Builder(
-            builder: (context) => Scaffold(
-                appBar: AppBar(
-                  centerTitle: true,
-                  title: const Text("加载字体"),
-                ),
-                body: const Center(
-                  child: Text(
-                    "华康字体",
-                    style: TextStyle(fontFamily: "huakang", fontSize: 36),
-                  ),
-                ))),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "加载字体",
+      home: Builder(
+          builder: (context) => Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: const Text("加载字体"),
+              ),
+              body: const DefaultTextStyle(
+                  style: TextStyle(color: Colors.red, fontSize: 36),
+                  textAlign: TextAlign.start,
+                  child: Column(
+                      spacing: 12,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Center(
+                          child: Text(
+                            "华康字体",
+                            style: TextStyle(fontFamily: "huakang"),
+                          ),
+                        ),
+                        Text("使用默认字体样式"),
+                        Text("略微调整",
+                            style:
+                                TextStyle(inherit: false, color: Colors.green))
+                      ])))),
+    );
+  }
 }
